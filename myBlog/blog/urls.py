2b4c61d2dc1abea90afef2bc33d7bbf253c765dd
Zakhar <zaxar24072003@icloud.com>
2020-04-03 02:57:41 +0300

    My Django Girls app, first commit

---
+++ myBlog/blog/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+ 
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from blog import admin
+from django.conf.urls import include
+ 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<int:id>/', views.single, name='single'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += staticfiles_urlpatterns()
+
+    if settings.MEDIA_ROOT:
+ 
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+# Эта строка опциональна и будет добавлять url'ы только при DEBUG = True
+ 
